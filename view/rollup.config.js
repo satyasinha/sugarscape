0eb9bd8556e157903e53d1ea883fa75338107211
satyasinha <satyendra.n.sinha@gmail.com>
2021-01-31 12:42:31 +1100

    :heavy_plus_sign::heavy_minus_sign:
    - added svelte-preprocess
    - added carbon-components-svelte
    - removed rollup-plugin-livereload
    - removed sirv-cli

--- view/rollup.config.js
+++ view/rollup.config.js
@@ -1,32 +1,32 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	let server;
+// function serve() {
+// 	let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+// 	function toExit() {
+// 		if (server) server.kill(0);
+// 	}
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+// 	return {
+// 		writeBundle() {
+// 			if (server) return;
+// 			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+// 				stdio: ['ignore', 'inherit', 'inherit'],
+// 				shell: true
+// 			});
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
+// 			process.on('SIGTERM', toExit);
+// 			process.on('exit', toExit);
+// 		}
+// 	};
+// }
 
 export default {
 	input: 'src/main.js',
@@ -38,6 +38,7 @@ export default {
 	},
 	plugins: [
 		svelte({
+			preprocess: sveltePreprocess(),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
@@ -58,19 +59,19 @@ export default {
 		}),
 		commonjs(),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+		// // In dev mode, call `npm run start` once
+		// // the bundle has been generated
+		// !production && serve(),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+		// // Watch the `public` directory and refresh the
+		// // browser on changes when not in production
+		// !production && livereload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
 		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+	]
+	// watch: {
+	// 	clearScreen: false
+	// }
 };
--- view/src/App.svelte
+++ view/src/App.svelte
@@ -1,4 +1,8 @@
 <script>
+  /** Gray 10 theme **/
+  import { Button } from "carbon-components-svelte";
+  import { Toggle } from "carbon-components-svelte";
+
   let rand = -1;
   function getRand() {
     fetch("./rand")
@@ -9,29 +13,32 @@
 </script>
 
 <main>
-	<h1>Satya your number is {rand}!!!!!!</h1>
-	<button on:click={getRand}>Get a random number</button>
+  <h1>Satya your number is {rand}!!!!!!</h1>
+  <Button on:click={getRand} kind="secondary">Random number</Button>
+  <Toggle labelText="Push notifications" />
 </main>
 
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  /** Carbon Gray 10 theme **/
+  @import "carbon-components-svelte/css/g10";
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
